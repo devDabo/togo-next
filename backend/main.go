b68bc6677b66ddb52da79a605c3cc8af3652f8c4
c1673142 <raj.chand.99@icloud.com>
2024-12-23 20:42:52 +0000

    added Update and Delete HTTP methods

--- backend/main.go
+++ backend/main.go
@@ -32,11 +32,12 @@ func main() {
 	router.HandleFunc("/api/go/todo", getTodos(db)).Methods("GET")
 	router.HandleFunc("/api/go/todo", createTodo(db)).Methods("POST")
 	router.HandleFunc("/api/go/todo/{id}", getTodo(db)).Methods("GET")
+	router.HandleFunc("/api/go/todo/{id}", updateTodo(db)).Methods("PUT")
+	router.HandleFunc("/api/go/todo/{id}", deleteTodo(db)).Methods("DELETE")
 
 	enhancedRouter := enableCORS(jsonContentTypeMiddleware(router))
 	log.Fatal(http.ListenAndServe(":8000", enhancedRouter))
 }
-
 
 func enableCORS(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -114,6 +115,48 @@ func getTodo(db *sql.DB) http.HandlerFunc {
 		}
 
 		json.NewEncoder(w).Encode(t)
+	}
+}
+
+func updateTodo(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var t Todo
+		json.NewDecoder(r.Body).Decode(&t)
+
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		_, err := db.Exec("UPDATE todo SET todo = $1 WHERE id=$2", t.Todo, id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var updatedTodo Todo
+		err = db.QueryRow("SELECT id, todo FROM todo WHERE id=$1", id).Scan(&updatedTodo.Id, &updatedTodo.Todo)
+		if err != nil {
+			log.Fatal(err)
+		}
+		json.NewEncoder(w).Encode(updatedTodo)
+	}
+}
+
+func deleteTodo(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		var t Todo
+		err := db.QueryRow("SELECT * FROM todo WHERE id=$1", id).Scan(&t.Id, &t.Todo)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		} else {
+			_, err := db.Exec("DELETE FROM todo WHERE id=$1", id)
+			if err != nil {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+			json.NewEncoder(w).Encode("Todo deleted")
+		}
 	}
 }
 
